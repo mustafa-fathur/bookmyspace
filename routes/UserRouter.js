d2f123f474b0b4859771ea0b016f5f9494a1ba45
Lauraif14 <145311957+Lauraif14@users.noreply.github.com>
2024-06-24 07:51:53 +0000

    done

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -7,11 +7,17 @@ const peminjaman = require('../controllers/PeminjamanController')
 const upload = require('../utils/UploadFileHandler')
 
 router.get('/dashboard', authMiddleware, permissionUser("user"), user.dashboard);
+router.get('/daftar-ruangan', authMiddleware, permissionUser("user"), ruangan.daftarRuangan);
+router.get('/cari-ruangan', authMiddleware, permissionUser("user"), ruangan.cariRuangan);
+router.get('/detail-ruangan/:id', authMiddleware, permissionUser("user"), ruangan.detailRuangan);
+router.get('/pinjam-ruangan/:idRuangan', authMiddleware, permissionUser("user"), peminjaman.formPinjamRuangan);
+router.post('/pinjam-ruangan/:idRuangan', authMiddleware, permissionUser("user"), upload.single('formulir'), peminjaman.pinjamRuangan);
+router.post('/batal-peminjaman/:idPeminjaman', authMiddleware, permissionUser("user"), peminjaman.batalPeminjaman);
+router.get('/download-riwayat', authMiddleware, permissionUser("user"), user.downloadRiwayat);
+router.get('/riwayat', authMiddleware, permissionUser("user"), user.riwayat);
 router.get('/profile', authMiddleware, permissionUser("user"), user.profile);
 router.post('/ubah-password', authMiddleware, permissionUser("user"), user.ubahPassword);
-router.get('/pinjam-ruangan/:idRuangan', authMiddleware, permissionUser("user"), peminjaman.formPinjamRuangan);
-router.get('/template-surat', authMiddleware, permissionUser("user"), ruangan.templateSurat);
-router.post('/pinjam-ruangan/:idRuangan', authMiddleware, permissionUser("user"), upload.single('formulir'), peminjaman.pinjamRuangan);
+
 
 
 module.exports = router;
